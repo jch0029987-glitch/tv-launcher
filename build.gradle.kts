23a093695fcebf9f2d451b183be23805f97577f4
jch0029987-glitch <jch0029987@gmail.com>
2026-01-27 11:33:41 -0600

    Update build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    // We define the versions here once
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+    id("app.cash.sqldelight") version "2.0.2" apply false
+}
