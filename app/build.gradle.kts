efb481dbfab64a57230b4ce5ba1d5b281dbfdbf3
jch0029987-glitch <jch0029987@gmail.com>
2026-01-27 11:43:12 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+kotlinOptions {
+        jvmTarget = "21"
+    }
 }
 
 sqldelight {
